b5ea0665e1722a9a60769fc14718b77bafe3ee66
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-02-02 10:47:48 -0500

    finish first pass condense_linked_list.py

--- gca_01/matrix_problem.py
+++ gca_01/matrix_problem.py
@@ -36,20 +36,22 @@ def resolve_matrix(matrix):
 
     for row in range(len(matrix)):
         for col in range(len(matrix[row])):
-            if (col % 4) == 0:
-                count = (count + 1) % num_tables
-
-            print(f"row={row}, col={col}, count={count}, el={matrix[row][col]}")
+            print(f"row={row}, col={col}, el={matrix[row][col]}")
+            print(f"(col//4)={col // 4}")
+            print(f"(col%4)={col % 4}")
+            print(f"(col-1)%4={(col - 1) % 4}")
+            tables[col//4][row][col%4] = matrix[row][col]
             # print(f"tables[{count}][{row}][{col % 4}]={tables[count][row][col % 4]}")
-            tables[count][row][(col - 1) % 4] = matrix[row][col]
+            # tables[col % 4][row][(col - 1) % 4] = matrix[row][col]
 
             # try:
             #     num_int = int(matrix[row][col])
             #     tables[count][row][col % 4] = num_int
             # except ValueError:
 
-    print(tables)
+    # print(tables)
     return tables
+
 
 if __name__ == '__main__':
     # driver code:
@@ -60,22 +62,28 @@ if __name__ == '__main__':
         ['13', '14', '15', '?']
     ]
     print(resolve_matrix(matrix1))
-    # -> [
-    #     ['1', '2', '3', '4']
-    #     ['5', '6', '7', '8']
-    #     ['9', '10', '11', '12']
-    #     ['13', '14', '15', '16']
-    # ]
-    # matrix2 = [
-    #     ['1', '2', '3', '4', '5', '6', '7', '8'],
-    #     ['5', '6', '7', '8', '9', '10', '11', '12'],
-    #     ['9', '10', '11', '12', '13', '14', '15', '16'],
-    #     ['13', '14', '15', '?', '1', '2', '3', '?']
-    # ]
-    # print(resolve_matrix(matrix2))
-    # # -> [
-    # #     ['1', '2', '3', '4', '5', '6', '7', '8'],
-    # #     ['5', '6', '7', '8', '9', '10', '11', '12'],
-    # #     ['9', '10', '11', '12', '13', '14', '15', '16'],
-    # #     ['13', '14', '15', '?', '1', '2', '3', '?']
-    # # ]
+    expected1 = [
+        ['1', '2', '3', '4'],
+        ['5', '6', '7', '8'],
+        ['9', '10', '11', '12'],
+        ['13', '14', '15', '16'],
+    ]
+    actual1 = resolve_matrix(matrix1)
+    print(f"TEST 2: \nactual: {actual1}, \nexpected: {expected1}")
+    print(f"\npassed: {actual1 == expected1}")
+    matrix2 = [
+        ['1', '2', '3', '4', '5', '6', '7', '8'],
+        ['5', '6', '7', '8', '9', '10', '11', '12'],
+        ['9', '10', '11', '12', '13', '14', '15', '16'],
+        ['13', '14', '15', '?', '1', '2', '3', '?'],
+    ]
+    expected2 = [
+        ['1', '2', '3', '4', '5', '6', '7', '8'],
+        ['5', '6', '7', '8', '9', '10', '11', '12'],
+        ['9', '10', '11', '12', '13', '14', '15', '16'],
+        ['13', '14', '15', '16', '1', '2', '3', '16'],
+    ]
+    actual2 = resolve_matrix(matrix2)
+    print(f"\n\n-----TEST 2: \n")
+    print(f"actual: {actual2}, \nexpected: {expected2}")
+    print(f"\npassed: {actual2 == expected2}")
---
+++ sprint_04/condense_linked_list/test_condense_linked_list.py
@@ -0,0 +1,29 @@
+import unittest
+"""
+TEST 1
+Input:
+node: [3, 4, 3, 2, 6, 1, 2, 6]
+Expected Output:
+[3, 4, 2, 6, 1]
+
+TEST 2
+Input:
+node: [1, 2, 1, 1, 2]
+Expected Output:
+[1, 2]
+
+T3
+Input:
+node: [1, 2, 2, 1, 3]
+Expected Output:
+[1, 2, 3]
+
+"""
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
