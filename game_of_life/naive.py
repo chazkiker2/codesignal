872c70d65ee631c6fb5bbcba5696b2c96028ed53
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-04-19 23:01:44 -0400

    refine first-pass solution

--- game_of_life/naive.py
+++ game_of_life/naive.py
@@ -1,16 +1,10 @@
-# x, y representation
-#
 # -1, 1     0, 1      1, 1
 # -1, 0,    0, 0      1, 0
 # -1, -1    0, -1     1, -1
 #
-# arr[x][y]
-#
 # [0][0]    [0][1]    [0][2]
 # [1][0]    [1][1]    [1][2]
 # [2][0]    [2][1]    [2][2]
-#
-# arr[i][j]
 #
 # [i-1][j-1]    [i-1][j  ]    [i-1][j+1]
 # [i  ][j-1]    [i=1][j=1]    [i  ][j+1]
@@ -80,7 +74,7 @@ def make_zilch_array(m, n):
 class Generation(list):
     def __init__(self, *args, **kwargs):
         super(Generation, self).__init__(*args, **kwargs)
-        self.current_gen = self.copy()
+        self.current_gen = list(sub.copy() for sub in self)
 
     def __str__(self):
         return "\n" + "\n".join("\t".join(str(el) for el in sub_array) for sub_array in self)
@@ -91,7 +85,6 @@ class Generation(list):
                 return True
         return False
 
-    # @staticmethod
     def get_neighbor_coords(self, i, j):
         M = len(self.current_gen)
         N = len(self.current_gen[0])
@@ -103,56 +96,29 @@ class Generation(list):
             "right": (i, j + 1),
             "down_right": (i + 1, j + 1),
             "down": (i + 1, j),
-            "down_left": (i + 1, j + 1),
+            "down_left": (i + 1, j - 1),
             "left": (i, j - 1),
         }
 
         return {k: (coords[0] % M, coords[1] % N) for k, coords in original.items()}
 
     def count_live_neighbors(self, i, j):
-        # print(f"{self.current_gen}, {self}")
-        M = len(self.current_gen)
-        N = len(self.current_gen[0])
-        num_live_neighbors = 0
-        visited = set()
-        neighbors = self.get_neighbor_coords(i, j)
-        # neighbors = [(ni, nj) for ni, nj in .values()]
-        # for direction, neighbor_coords in self.get_neighbor_coords(i, j).items():
-        for ni, nj in neighbors.values():
-            # ni, nj = neighbor_coords
-            ni %= M
-            nj %= N
-            stup = (ni, nj)
-            # and stup not in visited:
-            if self[ni][nj] != 0:
-                visited.add(stup)
-                # print(f"{ni=}, {nj=}")
-                num_live_neighbors += 1
-
+        neighbor_dict = self.get_neighbor_coords(i, j)
+        neighbors = [
+            (ni, nj) for ni, nj in neighbor_dict.values()
+            if self.current_gen[ni][nj] != 0
+        ]
+        num_live_neighbors = len(neighbors)
         return num_live_neighbors
 
     def generate_successor(self):
-        self.current_gen = self.copy()
-        current_gen = self.current_gen
-        # current generation given as argument
-        M = len(current_gen)
-        N = len(current_gen[0])
-
+        current_gen = self.current_gen = list(sub.copy() for sub in self)
         for i in range(len(current_gen)):
             for j in range(len(current_gen[i])):
-                # (i, j) represent our current cell coordinates
                 is_alive = current_gen[i][j] != 0
-                num_live_neighbors = 0
-                for direction, neighbor_coords in self.get_neighbor_coords(i, j).items():
-                    ni, nj = neighbor_coords
-                    ni %= M
-                    nj %= N
-                    if current_gen[ni][nj] != 0:
-                        num_live_neighbors += 1
-
+                num_live_neighbors = self.count_live_neighbors(i, j)
                 has_two = num_live_neighbors == 2
                 has_three = num_live_neighbors == 3
-
                 # 1. Any live cell with two or three live neighbors survives.
                 if is_alive and (has_two or has_three):
                     self[i][j] = 1
@@ -166,30 +132,25 @@ class Generation(list):
     def live(self):
         print(f"SEED GENERATION {self}")
 
-        last_self = self.copy()
-        count_reps = 1
+        last_self = [sub.copy() for sub in self]
         frame_count = 1
         stuck = False
 
-        while self.has_life():
-            # print(f"LAST GENERATION {self}")
+        while not stuck and frame_count < 2500:
             self.generate_successor()
             print(f"GENERATION {frame_count} {self}")
 
             if last_self == self:
-                count_reps += 1
-            else:
-                count_reps = 0
-
-            last_self = self.copy()
-            frame_count += 1
-
-            if count_reps == 4:
                 stuck = True
                 break
 
+            last_self = [sub.copy() for sub in self.copy()]
+            frame_count += 1
+
         if stuck:
             print(f"\nSTUCK AT THIS FRAME{current_generation}")
+        elif frame_count == 2500:
+            print("CAPPING OFF FRAMES AT 2500")
         else:
             print(f"\nNO LIFE TO CONTINUE\n{current_generation}")
 
@@ -201,20 +162,13 @@ if __name__ == "__main__":
         [0, 1, 1],
         [0, 0, 1],
     ]
-    # 0, 0, 1,
-    # 0, 1, 1,
-    # 0, 0, 1,
-    #
-    # 0,
-    #
+    # seed = [
+    #     [0, 0, 0, 0, 0],
+    #     [0, 0, 1, 0, 0],
+    #     [0, 0, 1, 0, 0],
+    #     [0, 0, 1, 0, 0],
+    #     [0, 0, 0, 0, 0],
+    # ]
 
     current_generation = Generation(seed)
     current_generation.live()
-
-    # while current_generation.has_life():
-    #     new_generation = Generation(generate_successor(current_generation))
-    #     print(current_generation)
-    #     print(new_generation)
-    #     current_generation = new_generation
-    # else:
-    #     print(f"\nNO LIFE TO CONTINUE\n{current_generation}")
--- game_of_life/test_naive.py
+++ game_of_life/test_naive.py
@@ -39,58 +39,81 @@ test_cases = {
             ],
         }
     },
-    2: {}
+    2: {
+        SEED: [
+            [0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0],
+        ],
+        NBRS: [
+            [0, 1, 1, 1, 0],
+            [0, 2, 1, 2, 0],
+            [0, 3, 2, 3, 0],
+            [0, 2, 1, 2, 0],
+            [0, 1, 1, 1, 0],
+        ],
+        FRAMES: {
+            0: [
+                [0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0],
+            ],
+            1: [
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 1, 1, 1, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+            ],
+        }
+    }
 }
 
-DEFAULT_SEED = test_cases[1][SEED]
+TEST_CASE = test_cases[2]
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.gen = Generation(DEFAULT_SEED)
+        self.test_case = TEST_CASE
+        self.gen = Generation(self.test_case[SEED])
 
     def test_get_neighbors(self):
+        M = len(self.gen) - 1
+        N = len(self.gen[0]) - 1
+
         expected = {
-            'up_left': (-1, -1),
-            'up': (-1, 0),
-            'up_right': (-1, 1),
+            'up_left': (M, N),
+            'up': (M, 0),
+            'up_right': (M, 1),
             'right': (0, 1),
             'down_right': (1, 1),
             'down': (1, 0),
-            'down_left': (1, 1),
-            'left': (0, -1)
+            'down_left': (1, N),
+            'left': (0, N)
         }
         actual = self.gen.get_neighbor_coords(0, 0)
 
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
-    def test_count_neighbors_00(self):
+    def make_tester_count_neighbors(self, i, j):
+        actual = self.gen.count_live_neighbors(i, j)
+        expected = self.test_case[NBRS][i][j]
+        msg = f"coords=({i}, {j}) {expected=}, {actual=}\n" \
+              f"{self.gen}\n"
+        self.assertEqual(expected, actual, msg)
 
-        dictx = {
-            (0, 0): 3,
-            (0, 1): 4,
-            (0, 2): 3,
-            (1, 0): 4,
-            (1, 1): 3,
-            (1, 2): 3,
-        }
+    def test_count_all_neighbors(self):
         for ti in range(len(self.gen)):
             for tj in range(len(self.gen[ti])):
-                actual = self.gen.count_live_neighbors(ti, tj)
-                expected = test_cases[1][NBRS][ti][tj]
-                msg = f"coords=({ti}, {tj}) {expected=}, {actual=}\n" \
-                      f"{self.gen=}"
-                self.assertEqual(expected, actual, msg)
-        # for coords, expected in dictx.items():
-        #     actual = self.gen.count_live_neighbors(*coords)
-        #     msg = f"{coords=} {expected=}, {actual=}\n" \
-        #           f"{self.gen=}"
-        #     self.assertEqual(expected, actual, msg)
+                self.make_tester_count_neighbors(ti, tj)
 
-        # expected = 3
-        # actual = self.gen.count_live_neighbors(0, 0)
-        # self.assertEqual(expected, actual)
+    def test_count_neighbors(self):
+        self.make_tester_count_neighbors(2, 1)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
