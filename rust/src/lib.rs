6e31d2364c976fff6d17212c8622b95b2c5c033b
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-06-22 20:15:13 -0500

    add rust lib

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,9 @@
+/// https://leetcode.com/problems/all-paths-from-source-to-target/
+struct LeetCode797;
+impl LeetCode797 {
+    pub fn all_paths_source_target(graph: Vec<Vec<i32>>) -> Vec<Vec<i32>> {}
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
---
+++ rust/target/rls/debug/deps/rust-e986084271c0021c.d
@@ -0,0 +1,5 @@
+/Users/chazadmin/code/projects/lambda/cs1/codesignal/rust/target/rls/debug/deps/rust-e986084271c0021c.rmeta: src/lib.rs
+
+/Users/chazadmin/code/projects/lambda/cs1/codesignal/rust/target/rls/debug/deps/rust-e986084271c0021c.d: src/lib.rs
+
+src/lib.rs:
