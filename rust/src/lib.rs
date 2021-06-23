909174f0a67d5a4952b91631500950b1cca40fd4
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-06-22 20:46:05 -0500

    add init rust setup; refactor python 'all paths' solution

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,13 +1,19 @@
+#[allow(dead_code)]
+use std::collections::HashMap;
+
 /// https://leetcode.com/problems/all-paths-from-source-to-target/
 struct LeetCode797;
 impl LeetCode797 {
-    pub fn all_paths_source_target(graph: Vec<Vec<i32>>) -> Vec<Vec<i32>> {}
+    #[allow(dead_code)]
+    pub fn all_paths_source_target(graph: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let map: HashMap<i32, Vec<i32>> = HashMap::new();
+    }
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn leet_code_797_001() {
         assert_eq!(2 + 2, 4);
     }
 }
