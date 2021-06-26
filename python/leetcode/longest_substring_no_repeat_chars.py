d03de1d9ed60b01810852d4e4c12acada3ab2ec5
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-06-26 16:48:50 -0500

    add better solution for longest substring

--- python/leetcode/longest_substring_no_repeat_chars.py
+++ python/leetcode/longest_substring_no_repeat_chars.py
@@ -2,20 +2,20 @@
 """
 Given a string `s`, find the length of the *longest substring* without repeating characters.
 
-## Example 1:
+# Example 1:
 
 - Input: s = "abcabcbb"
 - Output: 3
 - Explanation: The answer is "abc", with the length of 3.
 
 
-## Example 2:
+# Example 2:
 
 - Input: s = "bbbbb"
 - Output: 1
 - Explanation: The answer is "b", with the length of 1.
 
-## Example 3:
+# Example 3:
 
 - Input: s = "pwwkew"
 - Output: 3
@@ -23,13 +23,13 @@ Given a string `s`, find the length of the *longest substring* without repeating
 
 Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
-## Example 4:
+# Example 4:
 
 - Input: s = ""
 - Output: 0
 
 
-## Constraints:
+# Constraints:
 
 - `0 <= s.length <= 5 * 104`
 - `s` consists of English letters, digits, symbols and spaces
@@ -38,6 +38,22 @@ import unittest
 
 
 class Solution(object):
+    def length_of_longest_sub(self, s):
+        # a dict with key value pairs such that
+        # key: character -- value: the most recently visited index where we visited the character
+        dic = {}
+        # the length of the longest substring
+        max_len = start = 0
+
+        for current_index, char in enumerate(s):
+            if char in dic:
+                start = max(start, dic[char] + 1)
+
+            max_len = max(max_len, current_index - start + 1)
+            dic[char] = current_index
+
+        return max_len
+
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
@@ -65,7 +81,7 @@ class Test(unittest.TestCase):
     def setUp(self) -> None:
         super().setUp()
         solution = Solution()
-        self.fn = solution.lengthOfLongestSubstring
+        self.fn = solution.length_of_longest_sub
 
     def test_001(self):
         self.assertEqual(3, self.fn("abcabcbb"))
